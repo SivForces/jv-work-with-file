e14977eb0fa93acd9809eaf96a121067cd23fa9c
Vitalii Levda <vitalii.levda@tul.cz>
2025-08-29 22:11:43 +0300

    formatting

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -11,7 +11,6 @@ public class WorkWithFile {
     public void getStatistic(String fromFileName, String toFileName) {
         int supplyAmount = 0;
         int buyAmount = 0;
-        int result = 0;
         File myFile = new File(toFileName);
         FileWriter fileWriter = null;
         try {
@@ -29,18 +28,21 @@ public class WorkWithFile {
         String strings = String.join(" ", lines);
         String[] split = strings.split(",");
         for (int i = 0; i < split.length; i++) {
-            if(i%2 != 0){
+            if (i % 2 != 0) {
                 switch (split[i - 1]) {
                     case "buy" -> buyAmount += Integer.parseInt(split[i]);
                     case "supply" -> supplyAmount += Integer.parseInt(split[i]);
+                    default -> {
+
+                    }
                 }
             }
         }
-        result = supplyAmount + buyAmount;
+        int result = supplyAmount + buyAmount;
         try {
-            bufferedWriter.write ("supply," + supplyAmount) ;
-            bufferedWriter.write ("buy," + buyAmount) ;
-            bufferedWriter.write ("result," + result);
+            bufferedWriter.write("supply," + supplyAmount);
+            bufferedWriter.write("buy," + buyAmount);
+            bufferedWriter.write("result," + result);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
